cb4532dbcb23f9090f260b5934d89e5c6270f555
bombsandbottles <harrison@zafrin.net>
2015-11-10 18:08:42 -0500

    normalized on the way

--- Code/mir_utils.py
+++ Code/mir_utils.py
@@ -4,3 +4,6 @@ import scipy
 
 def convert2db(x):
 	return 20 * np.log10(x)
+
+def normalize(x):
+	x = x/(np.amax(np.absolute(x)))
--- Code/normalize_data.py
+++ Code/normalize_data.py
@@ -1,4 +1,6 @@
 import feature_extraction
+import shutil
+import mir_utils
 import os, sys
 
 def normalize_multitracks(filepath):
@@ -10,40 +12,65 @@ def normalize_multitracks(filepath):
 	is normalized as normal, and then all other tracks are raised
 	the same amount of dB as the track with the highest peak-level.
 	
-	returns: data = audio in samples across time
-			 data_l / _r = left and right channels seperated
-			 fs = sample rate
-	 		 t = time vector corresponding to data
+	returns: A new directory of normalized data
 	"""	
+	peak_amplitudes = []
 	tracks = os.listdir(filepath)
+	norm_path = filepath + "/normalized_tracks"
+
+	# Create the normalized data folder to house all our normalized data
+	create_folder(norm_path)
+
+	# Scan tracks, and get the peak amplitudes of each track
     for track in tracks:
     	file_name, file_extension = os.path.splitext(track)
     	if file_extension == '.wav':
     		data_s, data_l, data_r, fs, t = feature_extraction.import_audio(filepath + "/" + track)
-    		"!!! TODO : grab peak audio level !!!"
+    		peak_amplitude = calc_peak(data_l, data_r)
+    		peak_amplitudes.append(peak_amplitude)
+
+   	# Find the highest amplitude track by index
+    idxs = np.argwhere(peak_amplitudes == np.amax(peak_amplitudes))
+    idxs = idxs.flatten().tolist()
+
+    # Normalize the highest amplitude tracks
+    track_num = 0
+    for track in tracks:
+    	file_name, file_extension = os.path.splitext(track)
+    	if file_extension == '.wav' and track_num in idxs:
+    		data_s, data_l, data_r, fs, t = feature_extraction.import_audio(filepath + "/" + track)
+    		write_path = (norm_path + "/" + file_name + "_normalized" + file_extension)
+    		librosa.output.write_wav(write_path, data_s, sr=44100, norm=True)
+    	track_num += 1
 
 
-def get_peak(data_l, data_r):
+def calc_peak(data_l, data_r):
 	"""
-	filepath: string pointing to audio file on disk
+	data_l, data_r: discrete audio arrays, left and right channels
 
-	This function takes a directory of .wav multi-track files 
-	and normalizes them such that the track with the highest peak-level
-	is normalized as normal, and then all other tracks are raised
-	the same amount of dB as the track with the highest peak-level.
+	Quick function to find peak audio level of a stereo multi-track
 	
 	returns: the peak_amplitude 
 	"""
 	peaks = []
-    peaks.append(np.amax(data_l))
-    peaks.append(np.amax(data_r))
+    peaks.append(np.amax(np.absolute(data_l)))
+    peaks.append(np.amax(np.absolute(data_r)))
     if peaks[0] == peaks[1]:
         peak_amplitude = peaks[0]
     else:
         peak_amplitude = np.amax(peaks)
 	return peak_amplitude
-        		
 
-        		
+def create_folder(filepath):
+	"""
+	Creates the normalization folder to house all the new
+	normalized data
+	"""
+    if os.path.exists(filepath):
+         shutil.rmtree(filepath)
+    os.makedirs(filepath)
+
+    # if not os.path.exists(filepath):
+    #     os.makedirs(filepath)
 
             
